a30b2bbde08e377f0c8480449953b0361ac356a5
Saurabh08712 <109817824+Saurabh08712@users.noreply.github.com>
2024-09-02 18:06:37 +0000

    learning datatypes concepts

---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treated as newer version of java script
+//alert(3+3) // it will not work because we are using node not browser
+console.log(3+
+    3
+)//not a good practice code should be readible
+console.log("saurabh");
+let name="saurabh";
+let age=18
+let isActive=true
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
