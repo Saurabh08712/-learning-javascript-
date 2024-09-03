37e37361bd599f1ea1802e405963c8711fb99038
Saurabh08712 <109817824+Saurabh08712@users.noreply.github.com>
2024-09-03 17:34:15 +0000

    math and number

---
+++ 01_basics/6_Number_and_Math.js
@@ -0,0 +1,15 @@
+// const score = 400
+
+// const balance=new Number(100)
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString());
+
+const num=23.897
+console.log(num.toPrecision(2));
+
+console.log(Math);
+
+
+
