8d3fcdde0f973b7f5fb25b12d49d87affa414e05
Saurabh08712 <109817824+Saurabh08712@users.noreply.github.com>
2024-09-03 12:05:08 +0000

    datatype summary

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,25 @@
+// let value=3
+// let negValue=-value
+// console.log(negValue);
+
+// let str1="saurabh"
+// let str2="tiwari"
+
+// let str3= str1+str2
+
+// console.log(str3);
+
+//comparison
+
+// console.log(2>1);
+
+// console.log("2">1);
+
+// console.log(null>=0);
+// console.log(null<=0);
+console.log("2"===2);
+
+
+
+
+
---
+++ 01_basics/datatypeSummary.js
@@ -0,0 +1,35 @@
+//#Primitive
+// 7 types string, Number, Boolean, null, undefined, Symbol, 
+
+
+
+//javaScript is dynamically typed
+
+// const isLoggedIn=false
+// const outsideTemp=null
+
+// const id=Symbol('123')
+// const anotherId=Symbol('123')// both are unique value
+
+// const bigNumber=3444555656959549n
+
+//Rrefrence type non primitive 
+// Arrays, objects, Functions
+
+// const heros=["shaktiman","nagraj","doga"];
+// //objects
+// let myObj={
+//     name:"saurabh",
+//     age:21,
+// }
+
+//functions
+
+//  const myFun=functions()
+//  {
+//     console.log("Hello World");
+    
+//  }
+
+ console.log(typeof bigNumber);
+ 
